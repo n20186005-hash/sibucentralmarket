c9cc949b042dc9f9c444cb7b5b2e4dd7a679da4c
n20186005-hash <n20186005@gmail.com>
2026-08-13 11:25:30 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,6 +20,7 @@ if (SITE_URL) {
 export default defineConfig({
   site: SITE_URL || undefined,
   output: 'static',
+  session: false,
   adapter: cloudflare({
     imageService: 'passthrough',
   }),
